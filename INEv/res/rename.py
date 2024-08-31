91eabde517a95ec6fe6c5754f58da87010a1d180
PMeran101 <p.meran@campus.tu-berlin.de>
2024-08-31 16:15:41 +0000

    csv update

--- INEv/res/rename.py
+++ INEv/res/rename.py
@@ -1,7 +1,7 @@
 import os
 
 # Define the suffix you want to add
-suffix = "_normal"  # Replace with your desired suffix
+suffix = "_fog"  # Replace with your desired suffix
 
 # Get the current directory
 current_directory = os.getcwd()
@@ -9,7 +9,7 @@ current_directory = os.getcwd()
 # Loop through all files in the current directory
 for filename in os.listdir(current_directory):
     # Check if the file is a CSV
-    if filename.endswith(".csv") and not filename.endswith(".csv"):
+    if filename.endswith(".csv") and not filename.endswith("_normal.csv") and not filename.endswith("_fog.csv"):
         # Split the file name and extension
         name, ext = os.path.splitext(filename)
         
