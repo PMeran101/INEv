217faf344f33b86c8a1836e9c2aa7d73bb6e3a61
PMeran101 <p.meran@campus.tu-berlin.de>
2024-09-25 23:44:22 +0200

    added central costs only node 0

--- INEv/scripts/conflicting_qwl.sh
+++ INEv/scripts/conflicting_qwl.sh
@@ -4,36 +4,36 @@ cd ../code
 	
   echo "Script started at: $(date)"
 
-  for k in 5 10 20 # qwl size
-  do 
+#   for k in 5 10 20 # qwl size
+#   do 
       for h in 6 10 15 20 25 # num event types
       do
           echo "Starting outer loop with k=$k and h=$h at: $(date)"
 
           Generate network and graph files
-          python3 generate_network.py -nw 20 -ner 0.5 -es 1.3 -ne "$h"
+          python3 generate_network.py -nw 50 -ner 0.5 -es 1.3 -ne "$h"
           python3 generate_graph.py
           python3 allPairs.py
      
           a=1
-          while [ "$a" -lt 60 ]
+          while [ "$a" -lt 5 ]
           do
               echo "  Starting inner loop iteration $a with k=$k at: $(date)"
               
-              python3 generate_qwls.py 6 "$k"	
+              python3 generate_qwls.py 6 10	
               python3 generate_selectivity.py 
               python3 write_config_single.py
               python3 determine_all_single_selectivities.py
               python3 generate_projections.py
               python3 combigen.py
-              python3 computePlanCosts_aug.py "QWL$k"
+              python3 computePlanCosts_aug.py "QWL10"
               
               echo "  Completed inner loop iteration $a with k=$k at: $(date)"
               a=$((a + 1))
           done
           
           echo "Completed outer loop with k=$k and h=$h at: $(date)"
-      done
+    #   done
   done
   
   echo "Script completed at: $(date)"
