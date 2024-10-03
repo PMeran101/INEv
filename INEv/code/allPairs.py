3fef3aef83ef15515a29a62b113b9692083fc681
PMeran101 <p.meran@campus.tu-berlin.de>
2024-10-03 12:00:51 +0200

    minor changes for allPairs and DiGraph

--- INEv/code/allPairs.py
+++ INEv/code/allPairs.py
@@ -15,7 +15,8 @@ import pickle
 import time
 import numpy as np
 import random as rd 
-
+import os
+print(os.getcwd())
 with open('graph',  'rb') as graph_file:
     G = pickle.load(graph_file)
 
@@ -25,14 +26,25 @@ allPairs = [[] for x in myNodes]
 
 def fillMyMatrice(me):  
     myDistances = []
-    for j in range(len(G.nodes)):            
-           myDistances.append(len(nx.shortest_path(G, me, j, method='dijkstra')) - 1)   
+    for j in range(len(G.nodes)):   
+        try:
+            # Attempt to find the shortest path between me and j
+            myDistances.append(len(nx.shortest_path(G, me, j, method='dijkstra')) - 1)
+        except nx.NetworkXNoPath:
+            # If no direct path exists, simulate travel via the cloud (node 0)
+            try:
+                # Find shortest path from me to the cloud (node 0) and j to the cloud (node 0)
+                distance_me_to_cloud = len(nx.shortest_path(G, me, 0, method='dijkstra')) - 1
+                distance_j_to_cloud = len(nx.shortest_path(G, j, 0, method='dijkstra')) - 1
+                # Simulate the distance via the cloud by summing both distances
+                myDistances.append(distance_me_to_cloud + distance_j_to_cloud)
+            except nx.NetworkXNoPath:
+                # If there is no path to the cloud, set a very high value or handle it appropriately
+                myDistances.append(float('inf'))  # You can use a large number or some other placeholder
     return (me, myDistances)
-
-pool = multiprocessing.Pool()
          
 
-
+pool = multiprocessing.Pool()
 start = time.time()
 result = pool.map(fillMyMatrice, myNodes)
 for i in result:
--- INEv/code/test_proj.sh
+++ INEv/code/test_proj.sh
@@ -1,4 +1,4 @@
-python3 generate_network.py -nw 50 -ner 0.5 -es 1.3 -ne 15 -mp 2
+python3 generate_network.py -nw 15 -ner 0.5 -es 1.3 -ne 15 -mp 2
 python3 generate_graph.py
 python3 allPairs.py
 python3 generate_qwls.py 6 10	
