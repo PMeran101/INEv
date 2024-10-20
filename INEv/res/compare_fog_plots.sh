a6b6f6e2152413b302bb174f678887e380c38942
PMeran101 <p.meran@campus.tu-berlin.de>
2024-10-20 15:34:21 +0200

    addded figures

--- INEv/res/compare_fog_plots.sh
+++ INEv/res/compare_fog_plots.sh
@@ -3,7 +3,7 @@
 # and allows for insights into the impact of graph density on Transmission Ratio.
 
 # Transmission Ratio vs Event Types
-# Part 1: MaxParents 2, 3, 4
+#Part 1: MaxParents 2, 3, 4
 python3 plot_generic.py -i OP_MaxParents_2.csv OP_MaxParents_3.csv OP_MaxParents_4.csv -x EventTypes -y TransmissionRatio -l MaxParents_2 MaxParents_3 MaxParents_4 -o Fig_TransmissionRatio_EventTypes_MaxParents_2to4.pdf
 # Part 2: MaxParents 5, 6, 7
 python3 plot_generic.py -i OP_MaxParents_5.csv OP_MaxParents_6.csv OP_MaxParents_7.csv -x EventTypes -y TransmissionRatio -l MaxParents_5 MaxParents_6 MaxParents_7 -o Fig_TransmissionRatio_EventTypes_MaxParents_5to7.pdf
@@ -17,3 +17,8 @@ python3 plot_generic.py -i OP_MaxParents_2.csv OP_MaxParents_3.csv OP_MaxParents
 python3 plot_generic.py -i OP_MaxParents_5.csv OP_MaxParents_6.csv OP_MaxParents_7.csv -x WorkloadSize -y TransmissionRatio -l MaxParents_5 MaxParents_6 MaxParents_7 -o Fig_TransmissionRatio_WorkloadSize_MaxParents_5to7.pdf
 # Extra: MaxParents 2 and 7 (drastic comparison)
 python3 plot_generic.py -i OP_MaxParents_2.csv OP_MaxParents_7.csv -x WorkloadSize -y TransmissionRatio -l MaxParents_2 MaxParents_7 -o Fig_TransmissionRatio_WorkloadSize_MaxParents_2and7.pdf
+
+python3 plot_generic.py -i OP_Query_Sizes_5.csv OP_Query_Sizes_10.csv OP_Query_Sizes_20.csv -x EventTypes -y TransmissionRatio -l Query_Size_5 Query_Size_10 Query_Size_20 -o Fig_TransmissionRatio_QuerySizes_MaxParents.pdf
+
+# Example 3: All MaxParents files
+python3 plot_bars.py -i OP_SweetSpot_MaxParents_2.csv OP_SweetSpot_MaxParents_3.csv OP_SweetSpot_MaxParents_4.csv OP_SweetSpot_MaxParents_5.csv OP_SweetSpot_MaxParents_6.csv OP_SweetSpot_MaxParents_7.csv OP_SweetSpot_MaxParents_8.csv OP_SweetSpot_MaxParents_9.csv OP_SweetSpot_MaxParents_10.csv OP_SweetSpot_MaxParents_11.csv OP_SweetSpot_MaxParents_12.csv OP_SweetSpot_MaxParents_13.csv OP_SweetSpot_MaxParents_14.csv OP_SweetSpot_MaxParents_15.csv -y TransmissionRatio -l MaxParents_2 MaxParents_3 MaxParents_4 MaxParents_5 MaxParents_6 MaxParents_7 MaxParents_8 MaxParents_9 MaxParents_10 MaxParents_11 MaxParents_12 MaxParents_13 MaxParents_14 MaxParents_15 -o Fig_Bar_TransmissionRatio_AllMaxParents.pdf
