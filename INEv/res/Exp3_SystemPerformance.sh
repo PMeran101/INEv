4775a4493a3487b509171e9165f8104d6d04835d
PMeran101 <p.meran@campus.tu-berlin.de>
2024-11-07 19:16:45 +0100

    small fix

--- INEv/res/Exp3_SystemPerformance.sh
+++ INEv/res/Exp3_SystemPerformance.sh
@@ -9,5 +9,5 @@ python3 plot_ComputationTime.py -i combined_for_latency.csv -l Latency -x EventT
 python3 plot_latency_bars.py -i combined_for_latency_scaled.csv -x Nodes -l Latency -y centralHopLatency MaxPushPullLatency -o Fig_Latency_Scaled.pdf
 
 #Fourth Figure in Experiment in Paper
-python3 plot_latency_bars.py -i combined_for_latency_scaled.csv -x Nodes -l Latency -y CombigenComputationTime PushPullTime -o Fig_ComputationTime_Scaled.pdf
+python3 plot_latency_bars.py -i combined_for_latency_scaled.csv -x Nodes -l ComputationTime -y CombigenComputationTime PushPullTime -o Fig_ComputationTime_Scaled.pdf
 python3 multi_variate_latency.py -i combined_for_latency_scaled.csv -l ComputationTime -x Nodes -y CombigenComputationTime PushPullTime -o Fig_ComputationTime_Scaled_whisker.pdf
