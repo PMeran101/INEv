fc908217f51f9bac79c557edcd328d022c66cb57
PMeran101 <p.meran@campus.tu-berlin.de>
2024-11-23 11:52:21 +0100

    data fpr latency

--- INEv/res/Exp3_SystemPerformance.sh
+++ INEv/res/Exp3_SystemPerformance.sh
@@ -5,6 +5,9 @@ python3 plot_Latency.py -i combined_for_latency.csv -x EventTypes -y centralHopL
 #Second Figure in Experiment in Paper
 python3 plot_ComputationTime.py -i combined_for_latency.csv -l Latency -x EventTypes -y CombigenComputationTime PushPullTime -o Fig_Computation_Time_Network_50.pdf
 
+python3 plot_latency_bars.py -i combined_for_latency.csv -x MaximumParents -l Latency -y centralHopLatency MaxPushPullLatency -o Fig_Latency_Density.pdf
+python3 multi_variate_latency.py -i combined_for_latency.csv -l Latency -x MaximumParents -y centralHopLatency MaxPushPullLatency -o Fig_Latency_Density_Whikser.pdf
+
 #Third Figure in Experiment in Paper
 python3 plot_latency_bars.py -i combined_for_latency_scaled.csv -x Nodes -l Latency -y centralHopLatency MaxPushPullLatency -o Fig_Latency_Scaled.pdf
 
