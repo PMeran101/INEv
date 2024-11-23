e48ca793bf03f4df2d252cbabe66144a170da05e
PMeran101 <p.meran@campus.tu-berlin.de>
2024-11-23 12:12:22 +0100

    addeda data and fig

--- INEv/res/Exp3_SystemPerformance.sh
+++ INEv/res/Exp3_SystemPerformance.sh
@@ -9,6 +9,10 @@ python3 plot_ComputationTime.py -i combined_for_latency.csv -l Latency -x EventT
     -y PlacementComputationTime PushPullTime -o Fig_Computation_Time_Network_50.pdf \
     --legend_labels "Operator placement" "Push pull" --x_label "Event types" --y_label "Computation time in s"
 
+python3 plot_latency_bars.py -i combined_for_latency.csv -l Latency -x MaximumParents \
+    -y centralHopLatency MaxPushPullLatency -o Fig_Latency_Density.pdf \
+    --legend_labels "Operator placement" "Push pull" --x_label "Maximum Parents" --y_label "Latency"
+
 #Third Figure in Experiment in Paper
 python3 plot_latency_bars.py -i combined_for_latency_scaled.csv -x Nodes -l Latency \
     -y centralHopLatency MaxPushPullLatency -o Fig_Latency_Scaled.pdf \
